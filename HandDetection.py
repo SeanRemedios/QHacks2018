5f97addd0d522ccba075258bce920fbb246e74c6
SeanRemedios <14sr84@queensu.ca>
2018-02-03 17:22:12 -0500

    Updated HandDetection for previous finger placements to find deltas so swipes can be detected

--- HandDetection.py
+++ HandDetection.py
@@ -43,6 +43,12 @@ cv2.createTrackbar('h', 'HSV_TrackBar',0,179,nothing)
 cv2.createTrackbar('s', 'HSV_TrackBar',0,255,nothing)
 cv2.createTrackbar('v', 'HSV_TrackBar',0,255,nothing)
 
+# To track finger movement
+previousFinger = [(-1,-1), (-1,-1), (-1,-1), (-1,-1), (-1,-1)]
+# Gesture size in pixels
+GESTURESIZE = 200
+MAX_DISTANCE = 600
+
 while(1):
 
 	#Measure execution time 
@@ -62,7 +68,7 @@ while(1):
 	
 	#Kernel matrices for morphological transformation    
 	kernel_square = np.ones((11,11),np.uint8)
-	kernel_ellipse= cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+	kernel_ellipse = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
 	
 	#Perform morphological transformations to filter out the background noise
 	#Dilation increase skin color area
@@ -71,9 +77,9 @@ while(1):
 	erosion = cv2.erode(dilation,kernel_square,iterations = 1)    
 	dilation2 = cv2.dilate(erosion,kernel_ellipse,iterations = 1)    
 	filtered = cv2.medianBlur(dilation2,5)
-	kernel_ellipse= cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(8,8))
+	kernel_ellipse = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(8,8))
 	dilation2 = cv2.dilate(filtered,kernel_ellipse,iterations = 1)
-	kernel_ellipse= cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+	kernel_ellipse = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
 	dilation3 = cv2.dilate(filtered,kernel_ellipse,iterations = 1)
 	median = cv2.medianBlur(dilation2,5)
 	ret,thresh = cv2.threshold(median,127,255,0)
@@ -87,14 +93,14 @@ while(1):
 	#cv2.imshow('Dilation',median)
 	
 	#Find Max contour area (Assume that hand is in the frame)
-	max_area=100
-	ci=0	
+	max_area = 100
+	ci = 0	
 	for i in range(len(contours)):
-		cnt=contours[i]
+		cnt = contours[i]
 		area = cv2.contourArea(cnt)
-		if(area>max_area):
-			max_area=area
-			ci=i  
+		if(area > max_area):
+			max_area = area
+			ci = i
 			
 	#Largest area contour 			  
 	cnts = contours[ci]
@@ -108,7 +114,6 @@ while(1):
 	
 	#Get defect points and draw them in the original image
 	FarDefect = []
-	Max_Points = []
 	for i in range(defects.shape[0]):
 		s,e,f,d = defects[i,0]
 		start = tuple(cnts[s][0])
@@ -125,10 +130,10 @@ while(1):
 	if moments['m00']!=0:
 		cx = int(moments['m10']/moments['m00']) # cx = M10/M00
 		cy = int(moments['m01']/moments['m00']) # cy = M01/M00
-	centerMass=(cx,cy)    
+	centerMass = (cx,cy)    
 	
 	#Draw center mass
-	#cv2.circle(frame,centerMass,7,[100,0,255],2)
+	cv2.circle(frame,centerMass,7,[100,0,255],2)
 	font = cv2.FONT_HERSHEY_SIMPLEX
 	#cv2.putText(frame,'Center',tuple(centerMass),font,2,(255,255,255),2)     
 	
@@ -149,7 +154,7 @@ while(1):
 	finger = []
 	for i in range(0,len(hull)-1):
 		if (np.absolute(hull[i][0][0] - hull[i+1][0][0]) > 80) or ( np.absolute(hull[i][0][1] - hull[i+1][0][1]) > 80):
-			if hull[i][0][1] <500 :
+			if hull[i][0][1] < 500:
 				finger.append(hull[i][0])
 	
 	#The fingertip points are 5 hull points with largest y coordinates  
@@ -161,15 +166,29 @@ while(1):
 	for i in range(0,len(fingers)):
 		distance = np.sqrt(np.power(fingers[i][0]-centerMass[0],2)+np.power(fingers[i][1]-centerMass[0],2))
 		fingerDistance.append(distance)
+
 		pointToPlace = (fingers[i][0], fingers[i][1])
-		cv2.circle(frame, pointToPlace, 10, [100,255,255], 3)
+		deltaX = previousFinger[i][0] - pointToPlace[0]
+		deltaY = previousFinger[i][1] - pointToPlace[1]
+		absDeltaX = np.absolute(deltaX)
+		absDeltaY = np.absolute(deltaY)
+		#Find the fingers and place a circle on them
+		#cv2.circle(frame, pointToPlace, 10, [100,255,255], 3)
+		cv2.circle(frame, pointToPlace, 10, [0,255,0], 3)
+		if absDeltaX >= GESTURESIZE or absDeltaY >= GESTURESIZE:
+			if absDeltaX > MAX_DISTANCE or absDeltaY > MAX_DISTANCE:
+				continue
+			#cv2.line(frame,previousFinger[i],pointToPlace,[0,255,0],1)
+			print ("Previous Finger: %s" % (previousFinger[i],) + " - Moved Finger: %s" % (pointToPlace,))
+		previousFinger[i] = pointToPlace
+			
 	
 	#Finger is pointed/raised if the distance of between fingertip to the center mass is larger
 	#than the distance of average finger webbing to center mass by 130 pixels
 	result = 0
 	for i in range(0,len(fingers)):
 		if fingerDistance[i] > AverageDefectDistance+130:
-			result = result +1
+			result = result + 1
 	
 	#Print number of pointed fingers
 	cv2.putText(frame,str(result),(100,100),font,2,(255,255,255),2)
@@ -190,8 +209,10 @@ while(1):
 	
 	#cv2.drawContours(frame,[hull],-1,(255,255,255),2)
 	
+	#Flip the image to mirror
+	frame = cv2.flip(frame, 1)
 	##### Show final image ########
-	cv2.imshow('Dilation',frame)
+	cv2.imshow('Greenland',frame)
 	###############################
 	
 	#Print execution time
